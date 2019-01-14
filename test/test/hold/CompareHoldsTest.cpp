2f09014eb06682bf75ae673eee52746d7effc77a
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-01-14 22:07:58 +0000

    Fix Test

--- test/test/hold/CompareHoldsTest.cpp
+++ test/test/hold/CompareHoldsTest.cpp
@@ -8,31 +8,31 @@ using UKControllerPlugin::Hold::CompareHolds;
 using ::testing::Test;
 
 namespace UKControllerPluginTest {
-    namespace InitialAltitude {
+    namespace Hold {
 
-        class CompareInitialAltitudesTest : public Test
+        class CompareHoldsTest : public Test
         {
             public:
                 CompareHolds compare;
         };
 
-        TEST_F(CompareInitialAltitudesTest, LessThanStringReturnsTrueIfLessThan)
+        TEST_F(CompareHoldsTest, LessThanStringReturnsTrueIfLessThan)
         {
             HoldingData hold = { "TIMBA", 6000, 15000, 50, 0 };
             EXPECT_TRUE(compare(hold, "TIMBB"));
         }
 
-        TEST_F(CompareInitialAltitudesTest, LessThanStructReturnsTrueIfLessThan)
+        TEST_F(CompareHoldsTest, LessThanStructReturnsTrueIfLessThan)
         {
             HoldingData hold = { "TIMBB", 6000, 15000, 50, 0 };
             EXPECT_TRUE(compare("TIMBA", hold));
         }
 
-        TEST_F(CompareInitialAltitudesTest, CompareReturnsTrueIfFirstLessThanLast)
+        TEST_F(CompareHoldsTest, CompareReturnsTrueIfFirstLessThanLast)
         {
             HoldingData hold1 = { "TIMBA", 6000, 15000, 50, 0 };
             HoldingData hold2 = { "TIMBB", 6000, 15000, 50, 0 };
             EXPECT_TRUE(compare(hold1, hold2));
         }
-    }  // namespace InitialAltitude
+    }  // namespace Hold
 }  // namespace UKControllerPluginTest
