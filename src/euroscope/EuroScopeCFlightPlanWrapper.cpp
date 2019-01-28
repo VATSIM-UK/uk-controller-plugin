c1c9d8e3e9c30eac717f031d37f8fbd4f5285488
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-01-28 20:09:18 +0000

    Validate Squawk

--- src/euroscope/EuroScopeCFlightPlanWrapper.cpp
+++ src/euroscope/EuroScopeCFlightPlanWrapper.cpp
@@ -1,7 +1,10 @@
 #include "pch/stdafx.h"
 #include "euroscope/EuroScopeCFlightPlanWrapper.h"
+#include "squawk/SquawkValidator.h"
 
 using UKControllerPlugin::Euroscope::EuroscopeExtractedRouteInterface;
+using UKControllerPlugin::Squawk::SquawkValidator;
+
 namespace UKControllerPlugin {
     namespace Euroscope {
 
@@ -83,13 +86,12 @@ namespace UKControllerPlugin {
         bool EuroScopeCFlightPlanWrapper::HasAssignedSquawk(void) const
         {
             std::string squawk = this->originalData.GetControllerAssignedData().GetSquawk();
-
-            return squawk != "0200" &&
+            return SquawkValidator::ValidSquawk(squawk),
+                squawk != "0200" &&
                 squawk != "2200" &&
                 squawk != "1200" &&
                 squawk != "2000" &&
-                squawk != "0000" &&
-                squawk != "";
+                squawk != "0000";
         }
 
         const bool EuroScopeCFlightPlanWrapper::HasControllerClearedAltitude(void) const
