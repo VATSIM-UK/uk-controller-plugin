de8d6f61264d5eb3de5156d9458ec0cacdb4afd3
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-02-01 17:04:54 +0000

    Sort Out Publicness Of HoldManager

--- src/hold/HoldManager.h
+++ src/hold/HoldManager.h
@@ -23,27 +23,31 @@ namespace UKControllerPlugin {
         {
             public:
 
-            HoldManager(void);
-            void AddHold(std::string hold);
-            void AddAircraftToHold(
-                UKControllerPlugin::Euroscope::EuroScopeCFlightPlanInterface & flightplan,
-                UKControllerPlugin::Euroscope::EuroScopeCRadarTargetInterface & radarTarget,
-                std::string hold
-            );
-            bool AircraftIsInHold(std::string hold, std::string callsign) const;
-            bool HasHold(std::string hold) const;
-            std::set<UKControllerPlugin::Hold::HoldingAircraft, UKControllerPlugin::Hold::CompareHoldingAircraft>
-                GetAircraftInHold(std::string hold) const;
-
-            void RemoveAircraftFromAnyHold(std::string callsign);
-            void RemoveHold(std::string hold);
-            void UpdateHoldingAircraft(UKControllerPlugin::Euroscope::EuroscopePluginLoopbackInterface & plugin);
-
-            // A map of holds to aircraft
-            std::map<
-                std::string,
+                HoldManager(void);
+                void AddHold(std::string hold);
+                void AddAircraftToHold(
+                    UKControllerPlugin::Euroscope::EuroScopeCFlightPlanInterface & flightplan,
+                    UKControllerPlugin::Euroscope::EuroScopeCRadarTargetInterface & radarTarget,
+                    std::string hold
+                );
+                bool AircraftIsInHold(std::string hold, std::string callsign) const;
+                bool HasHold(std::string hold) const;
                 std::set<UKControllerPlugin::Hold::HoldingAircraft, UKControllerPlugin::Hold::CompareHoldingAircraft>
-            > holdData;
+                    GetAircraftInHold(std::string hold) const;
+
+                void RemoveAircraftFromAnyHold(std::string callsign);
+                void RemoveHold(std::string hold);
+                void UpdateHoldingAircraft(UKControllerPlugin::Euroscope::EuroscopePluginLoopbackInterface & plugin);
+
+            private:
+                // A map of holds to aircraft
+                std::map<
+                    std::string,
+                    std::set<
+                        UKControllerPlugin::Hold::HoldingAircraft,
+                        UKControllerPlugin::Hold::CompareHoldingAircraft
+                    >
+                > holdData;
         };
 
     }  // namespace Hold
