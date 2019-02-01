fa23a724003090013e5cc0c1c7c0622ce54842c8
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-02-01 19:39:13 +0000

    Get DllInstance From WinApiInterface

--- src/windows/WinApiInterface.h
+++ src/windows/WinApiInterface.h
@@ -15,8 +15,8 @@ namespace UKControllerPlugin {
         {
         public:
             explicit WinApiInterface(HINSTANCE dllInstance)
+                : dllInstance(dllInstance)
             {
-                this->dllInstance = dllInstance;
             };
             virtual ~WinApiInterface(void) {}  // namespace Windows
             virtual bool CreateFolder(std::string folder) = 0;
@@ -27,6 +27,9 @@ namespace UKControllerPlugin {
                 UINT numFileTypes,
                 const COMDLG_FILTERSPEC * fileTypes
             ) const = 0;
+            HINSTANCE GetDllInstance(void) const {
+                return this->dllInstance;
+            };
             virtual std::string GetFullPathToLocalFile(std::string relativePath) const = 0;
             virtual bool OpenDialog(int dialogId, DLGPROC callback = NULL, LPARAM params = NULL) const = 0;
             virtual void OpenGeneralSettingsDialog(UKControllerPlugin::Euroscope::UserSetting & setting) const = 0;
@@ -36,8 +39,9 @@ namespace UKControllerPlugin {
             virtual std::string ReadFromFile(std::wstring filename, bool relativePath = true) = 0;
             virtual void WriteToFile(std::string filename, std::string data, bool truncate) = 0;
 
+        private:
             // DLL Instance for the plugin
-            HINSTANCE dllInstance;
+            const HINSTANCE dllInstance;
         };
     }  // namespace Windows
 }  // namespace UKControllerPlugin
