c79a343aa9cd97c0b2c645f2061f60d814186cbe
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-04-17 19:13:11 +0100

    Add Event Handler To Process QNH Changes

---
+++ src/metar/PressureMonitor.cpp
@@ -0,0 +1,52 @@
+#include "pch/stdafx.h"
+#include "metar/PressureMonitor.h"
+#include "metar/MetarParsingFunctions.h"
+
+using UKControllerPlugin::Euroscope::UserSetting;
+using UKControllerPlugin::Message::UserMessager;
+
+namespace UKControllerPlugin {
+    namespace Metar {
+
+
+        PressureMonitor::PressureMonitor(UserSetting & pluginUserSettings, UserMessager & userMessager)
+            : pluginUserSettings(pluginUserSettings), userMessager(userMessager)
+        {
+
+        }
+
+        /*
+            Return the QNH stored for a station.
+        */
+        std::string PressureMonitor::GetStoredQnh(std::string station) const
+        {
+            return this->qnhs.count(station) ? this->qnhs.at(station) : this->qnhNotStored;
+        }
+
+        /*
+            Update the stored QNH with that of a new METAR. Notify the user if the
+            QNH has changed.
+        */
+        void PressureMonitor::NewMetar(std::string station, std::string metar)
+        {
+            std::string newQnh = GetQnhString(metar);
+
+            // Couldn't find QNH in METAR
+            if (newQnh == noQnh) {
+                LogInfo("Unable to parse QNH from METAR for " + station + ": " + metar);
+                return;
+            }
+
+            if (!this->qnhs.count(station) || this->qnhs.at(station) == newQnh) {
+                this->qnhs[station] = newQnh;
+                return;
+            }
+
+            if (this->pluginUserSettings.GetBooleanEntry(this->userSettingKey, false)) {
+               // Send message
+            }
+
+            this->qnhs[station] = newQnh;
+        }
+    }  // namespace Metar
+}  // namespace UKControllerPlugin
---
+++ src/metar/PressureMonitor.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "metar/MetarEventHandlerInterface.h"
+#include "euroscope/UserSetting.h"
+#include "message/UserMessager.h"
+
+namespace UKControllerPlugin {
+    namespace Metar {
+
+        /*
+            Monitors changing QNHs as reported by EuroScope
+            and notifies the user if the QNH has changed.
+        */
+        class PressureMonitor : public MetarEventHandlerInterface
+        {
+            public:
+
+                PressureMonitor(
+                    UKControllerPlugin::Euroscope::UserSetting & pluginUserSettings,
+                    UKControllerPlugin::Message::UserMessager & userMessager
+                );
+                std::string GetStoredQnh(std::string station) const;
+                // Inherited via MetarEventHandlerInterface
+                void NewMetar(std::string station, std::string metar) override;
+
+                // String to return if no QNHn is stored
+                const std::string qnhNotStored = "NONE";
+
+                // The user setting key to use to determine whether or not a notifcation should be sent
+                const std::string userSettingKey = "notifyPressureChanges";
+
+
+                // The user setting description
+                const std::string userSettingDescription = "Notify On Pressure Changes At Airports";
+
+            private:
+                
+                // A map of airfield -> last recorded QNH.
+                std::map<std::string, std::string> qnhs;
+
+                // A place for retrieving and saving user settings
+                UKControllerPlugin::Euroscope::UserSetting & pluginUserSettings;
+                
+                // Interface with ES for sending messages to the user about pressure changes.
+                UKControllerPlugin::Message::UserMessager & userMessager;
+        };
+    }  // namespace Metar
+}  // namespace UKControllerPlugin
