e753ddcd321c93fb51123483e8798a9933b517a2
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-05-12 20:15:00 +0100

    Change MSVC Location

--- ci/install-mfc.ps1
+++ ci/install-mfc.ps1
@@ -3,5 +3,5 @@
 ##########################################################################################
 
 Write-Host "Installing MFC dependencies for Visual Studio Community 2019"
-&"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community" --add Microsoft.VisualStudio.Component.VC.ATLMFC --add Microsoft.VisualStudio.Component.VC.ATLMFC.Spectre --quiet | Out-Null
+&"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise" --add Microsoft.VisualStudio.Component.VC.ATLMFC --add Microsoft.VisualStudio.Component.VC.ATLMFC.Spectre --quiet | Out-Null
 Write-Host "Finished Installing MFC dependencies for Visual Studio Community 2019"
