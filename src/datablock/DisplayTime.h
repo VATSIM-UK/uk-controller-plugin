af7360860bf6b4d0c79680bfc8cc94b5ad18c71d
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-05-26 15:34:14 +0100

    Update DisplayTime To Update From Settings, Provide Unknown Formats
    DisplayTime now provides the "unknown" date format, for when other
    parts of the plugin just want to display a default value. It gets this
    from the user settings in the general settings dialog.

--- src/datablock/DisplayTime.h
+++ src/datablock/DisplayTime.h
@@ -1,21 +1,43 @@
 #pragma once
+#include "euroscope/UserSettingAwareInterface.h"
 
 namespace UKControllerPlugin {
     namespace Datablock {
 
         /*
             A class for converting times to string representations, providing
-            a wrapper for Howard Hinnant's date library.
+            a wrapper for Howard Hinnant's date library. Also provides format
+            for "missing times" - blank or --:--.
         */
-        class DisplayTime
+        class DisplayTime : public UKControllerPlugin::Euroscope::UserSettingAwareInterface
         {
             public:
                 std::string FromTimestamp(time_t time) const;
                 std::string FromSystemTime(void) const;
                 std::string FromTimePoint(std::chrono::system_clock::time_point tp) const;
+                inline std::string GetUnknownTimeFormat(void) const
+                {
+                    return this->useBlankTimeForUnknown 
+                        ? this->unknownTimeFormatBlank 
+                        : this->unknownTimeFormatDefault;
+                };
+
+                // Inherited via UserSettingAwareInterface
+                void UserSettingsUpdated(UKControllerPlugin::Euroscope::UserSetting & userSettings) override;
 
                 // The format for time strings
                 const std::string timeFormat = "%H:%M";
+
+                // The default format to use when times are unknown
+                const std::string unknownTimeFormatDefault = "--:--";
+
+                // The format to use for unknown times when the user has specifically stated that they want it blank
+                const std::string unknownTimeFormatBlank = "";
+
+            private:
+               
+                // Whether or not blank times should be used for unknown times.
+                bool useBlankTimeForUnknown = false;
         };
     }  // namespace Datablock
 }  // namespace UKControllerPlugin
