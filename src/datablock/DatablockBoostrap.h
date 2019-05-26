3010b3cc9c4f8a08641d1a9ed2a2a3b9c31e17f4
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-05-26 15:36:04 +0100

    Bootstrap DisplayTime Properly

---
+++ src/datablock/DatablockBoostrap.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "bootstrap/PersistenceContainer.h"
+
+namespace UKControllerPlugin {
+    namespace Datablock {
+
+        void BootstrapPlugin(UKControllerPlugin::Bootstrap::PersistenceContainer & container);
+    }  // namespace Datablock
+}  // namespace UKControllerPlugin
