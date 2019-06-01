3f2c1ec72f92a0c95052a2311d26ac5662863ab2
Andy Ford <AndyTWF@users.noreply.github.com>
2019-06-01 21:52:52 +0100

    Bump Version

--- src/update/PluginVersion.cpp
+++ src/update/PluginVersion.cpp
@@ -3,7 +3,7 @@
 
 namespace UKControllerPlugin {
     namespace Plugin {
-        const char * const PluginVersion::version = "1.2.1";
+        const char * const PluginVersion::version = "1.3.0";
         const char * const PluginVersion::title = "UK Controller Plugin";
         const char * const PluginVersion::author = "VATSIM UK";
         const char * const PluginVersion::copyright = "VATSIM United Kingdom Division";
