fc87cefcc1fa2ca1f0ade772cbe5ab824dab222c
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-09-27 22:19:38 +0100

    Fix Unzip

--- ci/install-boost.ps1
+++ ci/install-boost.ps1
@@ -1,12 +1,12 @@
 # Download boost
 $source = "https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.7z"
-$destination = "C:\Libraries\boost_1_70_0"
+$destination = "C:\Libraries\boost_1_70_0.7z"
 Invoke-WebRequest $source -OutFile $destination
 
 # Unzip it
 Set-Location -Path C:\Libraries\
 Get-ChildItem -Path C:\Libraries\
-7z x boost_1_70_0
+7z x boost_1_70_0.7z -oC:\Libraries\boost_1_70_0
 
 Set-Location -Path C:\Libraries\boost_1_70_0
 bootstrap.bat
