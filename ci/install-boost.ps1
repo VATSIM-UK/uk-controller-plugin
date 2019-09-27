bbb78a5adb0f389cb3bad3eec129c4bd9c8a9002
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-09-27 22:27:29 +0100

    Tidy Up Boost

--- ci/install-boost.ps1
+++ ci/install-boost.ps1
@@ -1,13 +1,22 @@
-# Download boost
-$source = "https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.7z"
-$destination = "C:\Libraries\boost_1_70_0.7z"
-Invoke-WebRequest $source -OutFile $destination
+try
+{
+	# Download boost
+	$source = "https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.7z"
+	$destination = "C:\Libraries\boost_1_70_0.7z"
+	Invoke-WebRequest $source -OutFile $destination
 
-# Unzip it
-Set-Location -Path C:\Libraries\
-7z x boost_1_70_0.7z -oC:\Libraries
-Get-ChildItem -Path C:\Libraries\boost_1_70_0
+	# Unzip it
+	Set-Location -Path C:\Libraries\
+	7z x boost_1_70_0.7z -oC:\Libraries
 
-Set-Location -Path C:\Libraries\boost_1_70_0
-.\bootstrap.bat
-.\b2.exe architecture=x86 address-model=32
+	# Run the boost build
+	Set-Location -Path C:\Libraries\boost_1_70_0
+	.\bootstrap.bat
+	.\b2.exe architecture=x86 address-model=32
+
+	Exit $lastExitCode;
+}
+catch
+{
+	Exit 1;
+}
