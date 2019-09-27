ebfd907580a78b6b64d9f2087f934e018e77260b
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-09-27 22:07:28 +0100

    Install Custom Boost Version On Appveyor

---
+++ ci/install-boost.ps1
@@ -0,0 +1,12 @@
+# Download boost
+$source = "https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.7z"
+$destination = "C:\Libraries\boost_1_70_0"
+Invoke-WebRequest $source -OutFile $destination
+
+# Unzip it
+Set-Location -Path C:\Libraries\
+7z x boost_1_70_0.7z
+
+Set-Location -Path C:\Libraries\boost_1_70_0
+bootstrap.bat
+b2.exe architecture=x86 address-model=32
