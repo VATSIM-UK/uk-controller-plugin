ae62fe1884e2ce2d0d70647d32cbe301c458d129
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-03-25 20:42:21 +0000

    Add Test For Bootstrap

---
+++ test/test/plugin/PluginInformationBootstrapTest.cpp
@@ -0,0 +1,52 @@
+#include "pch/pch.h"
+#include "bootstrap/PersistenceContainer.h"
+#include "plugin/PluginInformationBootstrap.h"
+#include "plugin/FunctionCallEventHandler.h"
+#include "command/CommandHandlerCollection.h"
+#include "radarscreen/ConfigurableDisplayCollection.h"
+
+using UKControllerPlugin::Plugin::BootstrapPluginInformationMessage;
+using UKControllerPlugin::Bootstrap::PersistenceContainer;
+using UKControllerPlugin::Plugin::FunctionCallEventHandler;
+using UKControllerPlugin::Command::CommandHandlerCollection;
+using UKControllerPlugin::RadarScreen::ConfigurableDisplayCollection;
+using ::testing::Test;
+using ::testing::NiceMock;
+using ::testing::_;
+
+namespace UKControllerPluginTest {
+    namespace Plugin {
+
+        class PluginInformationBootstrapTest : public Test
+        {
+            public:
+
+                PluginInformationBootstrapTest()
+                {
+                    container.pluginFunctionHandlers.reset(new FunctionCallEventHandler);
+                    container.commandHandlers.reset(new CommandHandlerCollection);
+                }
+
+                ConfigurableDisplayCollection configurableDisplays;
+                PersistenceContainer container;
+        };
+
+        TEST_F(PluginInformationBootstrapTest, ItRegistersAConfigurableDisplay)
+        {
+            BootstrapPluginInformationMessage(this->container, this->configurableDisplays);
+            EXPECT_EQ(1, this->configurableDisplays.CountDisplays());
+        }
+
+        TEST_F(PluginInformationBootstrapTest, ItRegistersACommandHandler)
+        {
+            BootstrapPluginInformationMessage(this->container, this->configurableDisplays);
+            EXPECT_EQ(1, this->container.commandHandlers->CountHandlers());
+        }
+
+        TEST_F(PluginInformationBootstrapTest, ItRegistersAFunctionCallHandler)
+        {
+            BootstrapPluginInformationMessage(this->container, this->configurableDisplays);
+            EXPECT_EQ(1, this->container.pluginFunctionHandlers->CountCallbacks());
+        }
+    }  // namespace Plugin
+}  // namespace UKControllerPluginTest
