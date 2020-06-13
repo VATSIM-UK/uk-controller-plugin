412e3ab8642f4beb287f70fd4bd3df9e48ab7e63
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-13 17:32:36 +0100

    Style Src

--- src/task/TaskRunnerInterface.h
+++ src/task/TaskRunnerInterface.h
@@ -12,7 +12,7 @@ namespace UKControllerPlugin {
         class TaskRunnerInterface
         {
             public:
-                virtual ~TaskRunnerInterface(void) {};
+                virtual ~TaskRunnerInterface(void) {}
                 virtual size_t CountThreads(void) const = 0;
                 virtual void QueueAsynchronousTask(std::function<void(void)> task) = 0;
         };
