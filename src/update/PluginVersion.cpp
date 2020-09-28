9272cc8968c6be0727e5d027c092d03842b3a107
Andy Ford <AndyTWF@users.noreply.github.com>
2020-09-28 18:29:12 +0100

    Update PluginVersion.cpp

--- src/update/PluginVersion.cpp
+++ src/update/PluginVersion.cpp
@@ -3,7 +3,7 @@
 
 namespace UKControllerPlugin {
     namespace Plugin {
-        const char * const PluginVersion::version = "2.4.0";
+        const char * const PluginVersion::version = "2.5.0";
         const char * const PluginVersion::title = "UK Controller Plugin";
         const char * const PluginVersion::author = "VATSIM UK";
         const char * const PluginVersion::copyright = "VATSIM United Kingdom Division";
