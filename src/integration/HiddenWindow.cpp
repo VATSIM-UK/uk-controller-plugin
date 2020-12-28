c5bc1141eb1c0e037cfe89bc1fd04383981c39ed
Andy Ford <andyford993@gmail.com>
2020-12-28 14:05:40 +0000

    Add Const

--- src/integration/HiddenWindow.cpp
+++ src/integration/HiddenWindow.cpp
@@ -6,6 +6,13 @@ namespace UKControllerPlugin {
     namespace Integration {
         ExternalMessageEventHandler* handler = nullptr;
 
+        /*
+         * When using COPYDATASTRUCT the dwData member is essentially an enum that dictates what kind of data is being
+         * sent. It can be anything. For UKCP we use a single number as a check to ensure that the data is intended
+         * for us and the handlers will parse the message to see what it actually is and process it.
+         */
+        const ULONG_PTR dataType = 811;
+
         LRESULT CALLBACK HiddenWindow(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         {
             switch (msg) {
@@ -18,7 +25,7 @@ namespace UKControllerPlugin {
                 case WM_COPYDATA: {
                     COPYDATASTRUCT* data = reinterpret_cast<COPYDATASTRUCT*>(lParam);
 
-                    if (data != nullptr && data->dwData == 811 && data->lpData != nullptr && handler != nullptr) {
+                    if (data != nullptr && data->dwData == dataType && data->lpData != nullptr && handler != nullptr) {
                         handler->AddMessageToQueue(reinterpret_cast<const char*>(data->lpData));
                     }
                     return TRUE;
