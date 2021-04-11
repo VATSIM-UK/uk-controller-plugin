aafc72a20f5f3145097cb2b8311073b212913bdd
Andy Ford <andyford993@gmail.com>
2021-04-11 16:39:55 +0100

    feat(websocket): create polling-style websocket
    Rather than use a proper websocket, switch it out for a poll to the API every 10 seconds. This is
    more reliable and is far more under our control than the current package.

--- src/websocket/PollingWebsocketConnection.h
+++ src/websocket/PollingWebsocketConnection.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "websocket/WebsocketConnectionInterface.h"
+#include "timedevent/AbstractTimedEvent.h"
 
 namespace UKControllerPlugin {
 
@@ -7,25 +8,72 @@ namespace UKControllerPlugin {
         class ApiInterface;
     } // namespace Api
 
+    namespace TaskManager {
+        class TaskRunnerInterface;
+    } // namespace TaskManager
+
     namespace Websocket {
         /*
             A class that imitates the websocket connection but actually gets its data
             through regular polling.
         */
-        class PollingWebsocketConnection : public WebsocketConnectionInterface
+        class PollingWebsocketConnection : public WebsocketConnectionInterface, public TimedEvent::AbstractTimedEvent
         {
             public:
-                explicit PollingWebsocketConnection(const Api::ApiInterface& api);
+                explicit PollingWebsocketConnection(
+                    const Api::ApiInterface& api,
+                    TaskManager::TaskRunnerInterface& taskRunner
+                );
 
                 // Inherited from WebsocketConnectionInterface
                 void WriteMessage(std::string message) override;
                 std::string GetNextMessage() override;
                 void SetIdleTimeout(std::chrono::seconds timeout) override;
+                void TimedEventTrigger() override;
+
+                static bool SyncResponseValid(const nlohmann::json& response);
+                static bool LatestPluginEventsResponseValid(const nlohmann::json& response);
+                static bool PluginEventValid(const nlohmann::json& pluginEvent);
+
+                std::chrono::system_clock::time_point LastPollTime() const;
+                void SetLastPollTime(std::chrono::system_clock::time_point lastPollTime);
+                void SetSyncInProgress();
+                void SetUpdateInProgress();
+                void SetSynced();
+                int LastEventId() const;
+
+                // How often we should poll for new updates
+                const std::chrono::duration<int64_t> pollInterval = std::chrono::seconds(10);
 
             private:
 
+                void SyncEvents();
+                void GetLatestEvents();
+
+                // Whether or not we're currently syncing events
+                bool syncInProgress = false;
+
+                // Whether or not we're currently in the process of retrieving the latest events
+                bool updateInProgress = false;
+
+                // The last event that we processed
+                int lastEventId = -1;
+
+                // The time that we last made an attempt to poll - default it to one second after the poll interval.
+                std::chrono::system_clock::time_point lastPollTime = std::chrono::system_clock::now() -
+                    pollInterval - std::chrono::seconds(1);
+
                 // The api for polling for updates
                 const Api::ApiInterface& api;
+
+                // Allows polls to be run asynchronously
+                TaskManager::TaskRunnerInterface& taskRunner;
+
+                // Messages that are yet to be processed by the rest of the plugin
+                std::queue<std::string> inboundMessages;
+
+                // Protects the inbound messages queue
+                std::mutex inboundMessageQueueGuard;
         };
     }  // namespace Websocket
 }  // namespace UKControllerPlugin
--- test/test/websocket/PollingWebsocketConnectionTest.cpp
+++ test/test/websocket/PollingWebsocketConnectionTest.cpp
@@ -1,20 +1,379 @@
 #include "pch/pch.h"
 #include "websocket/PollingWebsocketConnection.h"
+#include "mock/MockTaskRunnerInterface.h"
+#include "mock/MockApiInterface.h"
+#include "api/ApiException.h"
 
 using ::testing::Test;
+using testing::NiceMock;
+using testing::Return;
+using testing::Throw;
 using UKControllerPlugin::Websocket::PollingWebsocketConnection;
+using UKControllerPluginTest::Api::MockApiInterface;
+using UKControllerPluginTest::TaskManager::MockTaskRunnerInterface;
+using UKControllerPlugin::Api::ApiException;
 
 namespace UKControllerPluginTest {
     namespace Websocket {
 
         class PollingWebsocketConnectionTest : public Test
         {
+            public:
+                PollingWebsocketConnectionTest()
+                    : connection(mockApi, mockTaskRunner)
+                { }
 
+                MockTaskRunnerInterface mockTaskRunner;
+                NiceMock<MockApiInterface> mockApi;
+                PollingWebsocketConnection connection;
         };
 
-        TEST_F(PollingWebsocketConnectionTest, ItDoesSomething)
+        TEST_F(PollingWebsocketConnectionTest, GetNextMessageReturnsEmptyIfNoMessages)
         {
-            EXPECT_TRUE(true);
+            EXPECT_EQ("", connection.GetNextMessage());
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, SyncResponseIsValidIfAllPresent)
+        {
+            nlohmann::json response = {
+                {"event_id", 55}
+            };
+            EXPECT_TRUE(connection.SyncResponseValid(response));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, SyncResponseIsValidIfEventIdNotInteger)
+        {
+            nlohmann::json response = {
+                {"event_id", "55"}
+            };
+            EXPECT_FALSE(connection.SyncResponseValid(response));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, SyncResponseIsValidIfEventIdMissing)
+        {
+            nlohmann::json response = {
+                {"not_event_id", 55}
+            };
+            EXPECT_FALSE(connection.SyncResponseValid(response));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, LatestPluginEventsResponseIsValidIfArray)
+        {
+            EXPECT_TRUE(connection.LatestPluginEventsResponseValid(nlohmann::json::array()));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, LatestPluginEventsResponseIsNotValidIfNotArray)
+        {
+            EXPECT_FALSE(connection.LatestPluginEventsResponseValid(nlohmann::json::object()));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, PluginEventIsValidIfAllPresent)
+        {
+            nlohmann::json pluginEvent = {
+                {"id", 55},
+                {"event", nlohmann::json{{"data", nlohmann::json{{"foo", "bar"}}}}}
+            };
+
+            EXPECT_TRUE(connection.PluginEventValid(pluginEvent));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, PluginEventIsNotValidIfEventDataNotObject)
+        {
+            nlohmann::json pluginEvent = {
+                {"id", 55},
+                {"event", nlohmann::json{{"data", nlohmann::json::array()}}}
+            };
+
+            EXPECT_FALSE(connection.PluginEventValid(pluginEvent));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, PluginEventIsNotValidIfEventHasNoData)
+        {
+            nlohmann::json pluginEvent = {
+                {"id", 55},
+                {"event", nlohmann::json::object()}
+            };
+
+            EXPECT_FALSE(connection.PluginEventValid(pluginEvent));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, PluginEventIsNotValidIfEventNotObject)
+        {
+            nlohmann::json pluginEvent = {
+                {"id", 55},
+                {"event", nlohmann::json::array()}
+            };
+
+            EXPECT_FALSE(connection.PluginEventValid(pluginEvent));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, PluginEventIsNotValidIfNoEvent)
+        {
+            nlohmann::json pluginEvent = {
+                {"id", 55},
+            };
+
+            EXPECT_FALSE(connection.PluginEventValid(pluginEvent));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, PluginEventIsNotValidIfIdNotAnInteger)
+        {
+            nlohmann::json pluginEvent = {
+                {"id", "55"},
+                {"event", nlohmann::json{{"data", nlohmann::json{{"foo", "bar"}}}}}
+            };
+
+            EXPECT_FALSE(connection.PluginEventValid(pluginEvent));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, PluginEventIsNotValidIfNoId)
+        {
+            nlohmann::json pluginEvent = {
+                {"event", nlohmann::json{{"data", nlohmann::json{{"foo", "bar"}}}}}
+            };
+
+            EXPECT_FALSE(connection.PluginEventValid(pluginEvent));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, ItHandlesApiExceptionsWhenSyncing)
+        {
+            EXPECT_CALL(mockApi, SyncPluginEvents())
+                .Times(1)
+                .WillOnce(Throw(ApiException("foo")));
+
+            connection.TimedEventTrigger();
+            EXPECT_TRUE(std::chrono::system_clock::now() - connection.LastPollTime() < std::chrono::seconds(2));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, ItHandlesInvalidResponseWhenSyncing)
+        {
+            EXPECT_CALL(mockApi, SyncPluginEvents())
+                .Times(1)
+                .WillOnce(Return(nlohmann::json::array()));
+
+            connection.TimedEventTrigger();
+            EXPECT_TRUE(std::chrono::system_clock::now() - connection.LastPollTime() < std::chrono::seconds(2));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, ItSyncsTheLastEventId)
+        {
+            nlohmann::json syncResponse{
+                {"event_id", 55}
+            };
+
+            EXPECT_CALL(mockApi, SyncPluginEvents())
+                .Times(1)
+                .WillOnce(Return(syncResponse));
+
+            connection.TimedEventTrigger();
+            EXPECT_TRUE(connection.LastPollTime() < std::chrono::system_clock::now() - connection.pollInterval);
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, ItPushesAConnectionEstablishedMessageWhenSyncComplete)
+        {
+            nlohmann::json syncResponse{
+                {"event_id", 55}
+            };
+
+            EXPECT_CALL(mockApi, SyncPluginEvents())
+                .Times(1)
+                .WillOnce(Return(syncResponse));
+
+            connection.TimedEventTrigger();
+
+            std::string expectedMessage = nlohmann::json({
+                {"event", "pusher:connection_established"},
+                {"data", nlohmann::json({{"socket_id", "abc.def"}, {"activity_timeout", 30}}).dump()}
+            }).dump();
+            EXPECT_EQ(expectedMessage, connection.GetNextMessage());
+            EXPECT_EQ("", connection.GetNextMessage());
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, ItDoesntSyncIfSyncInProgress)
+        {
+            connection.SetSyncInProgress();
+
+            EXPECT_CALL(mockApi, SyncPluginEvents())
+                .Times(0);
+
+            connection.TimedEventTrigger();
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, TestItDoesntSyncIfRecentlyPolled)
+        {
+            connection.SetLastPollTime(std::chrono::system_clock::now());
+
+            EXPECT_CALL(mockApi, SyncPluginEvents())
+                .Times(0);
+
+            connection.TimedEventTrigger();
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, ItHandlesApiExceptionWhenUpdating)
+        {
+            connection.SetSynced();
+
+            EXPECT_CALL(mockApi, GetLatestPluginEvents(0))
+                .Times(1)
+                .WillOnce(Throw(ApiException("foo")));
+
+            connection.TimedEventTrigger();
+            EXPECT_TRUE(std::chrono::system_clock::now() - connection.LastPollTime() < std::chrono::seconds(2));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, ItHandlesInvalidApiResponseWhenUpdating)
+        {
+            connection.SetSynced();
+
+            EXPECT_CALL(mockApi, GetLatestPluginEvents(0))
+                .Times(1)
+                .WillOnce(Return(nlohmann::json::object()));
+
+            connection.TimedEventTrigger();
+            EXPECT_TRUE(std::chrono::system_clock::now() - connection.LastPollTime() < std::chrono::seconds(2));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, ItUpdatesLastEventIdWhenUpdating)
+        {
+            nlohmann::json response = nlohmann::json::array({
+                {
+                    {"id", 3},
+                    {
+                        "event",
+                        nlohmann::json{
+                            {"channel", "foo"},
+                            {"data", nlohmann::json{{"bar", "baz"}}}
+                        }
+                    }
+                },
+                {
+                    {"id", 7},
+                    {
+                        "event",
+                        nlohmann::json{
+                            {"channel", "bish"},
+                            {"data", nlohmann::json{{"bash", "bosh"}}}
+                        }
+                    }
+                }
+            });
+
+            connection.SetSynced();
+
+            EXPECT_CALL(mockApi, GetLatestPluginEvents(0))
+                .Times(1)
+                .WillOnce(Return(response));
+
+            connection.TimedEventTrigger();
+
+            EXPECT_EQ(7, connection.LastEventId());
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, ItSetsLastPollTimeFollowingUpdate)
+        {
+            nlohmann::json response = nlohmann::json::array({
+                {
+                    {"id", 3},
+                    {
+                        "event",
+                        nlohmann::json{
+                            {"channel", "foo"},
+                            {"data", nlohmann::json{{"bar", "baz"}}}
+                        }
+                    }
+                },
+                {
+                    {"id", 7},
+                    {
+                        "event",
+                        nlohmann::json{
+                            {"channel", "bish"},
+                            {"data", nlohmann::json{{"bash", "bosh"}}}
+                        }
+                    }
+                }
+            });
+
+            connection.SetSynced();
+
+            EXPECT_CALL(mockApi, GetLatestPluginEvents(0))
+                .Times(1)
+                .WillOnce(Return(response));
+
+            connection.TimedEventTrigger();
+
+            EXPECT_TRUE(std::chrono::system_clock::now() - connection.LastPollTime() < std::chrono::seconds(2));
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, TestItProcessesWebsocketMessages)
+        {
+            nlohmann::json response = nlohmann::json::array({
+                {
+                    {"id", 3},
+                    {
+                        "event",
+                        nlohmann::json{
+                            {"channel", "foo"},
+                            {"data", nlohmann::json{{"bar", "baz"}}}
+                        }
+                    }
+                },
+                {
+                    {"id", 7},
+                    {
+                        "event",
+                        nlohmann::json{
+                            {"channel", "bish"},
+                            {"data", nlohmann::json{{"bash", "bosh"}}}
+                        }
+                    }
+                }
+            });
+
+            connection.SetSynced();
+
+            EXPECT_CALL(mockApi, GetLatestPluginEvents(0))
+                .Times(1)
+                .WillOnce(Return(response));
+
+            connection.TimedEventTrigger();
+
+
+            std::string firstExpectedMessage = nlohmann::json{
+                {"channel", "foo"},
+                {"data", nlohmann::json{{"bar", "baz"}}.dump()}
+            }.dump();
+            EXPECT_EQ(firstExpectedMessage, connection.GetNextMessage());
+
+            std::string secondExpectedMessage = nlohmann::json{
+                {"channel", "bish"},
+                {"data", nlohmann::json{{"bash", "bosh"}}.dump()}
+            }.dump();
+            EXPECT_EQ(secondExpectedMessage, connection.GetNextMessage());
+
+            EXPECT_EQ("", connection.GetNextMessage());
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, TestItDoesntUpdateIfUpdateInProgress)
+        {
+            connection.SetSynced();
+            connection.SetUpdateInProgress();
+
+            EXPECT_CALL(mockApi, GetLatestPluginEvents(0))
+                .Times(0);
+
+            connection.TimedEventTrigger();
+        }
+
+        TEST_F(PollingWebsocketConnectionTest, TestItDoesntUpdateIfRecentlyPolled)
+        {
+            connection.SetSynced();
+            connection.SetLastPollTime(std::chrono::system_clock::now());
+
+            EXPECT_CALL(mockApi, GetLatestPluginEvents(0))
+                .Times(0);
+
+            connection.TimedEventTrigger();
         }
     }  // namespace Websocket
 }  // namespace UKControllerPluginTest
