a95fbc35251b6f0973eaa8f6529fa6f996f490be
Andy Ford <andyford993@gmail.com>
2021-04-25 18:42:48 +0100

    revert(bootstrap): remove copy files between root folders
    This was useful to move people over silently, but given this will be version 3.0.0, its a good
    opportunity.
    
    BREAKING CHANGE: Removed file copies, old users who havent used the plugin the past four months will
    have to move back over manually.

--- src/bootstrap/CopyFilesToNewFolder.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "windows/WinApiInterface.h"
-
-namespace UKControllerPlugin {
-    namespace Bootstrap {
-        void CopyFilesToNewFolder(UKControllerPlugin::Windows::WinApiInterface & windows);
-    }  // namespace Bootstrap
-}  // namespace UKControllerPlugin
