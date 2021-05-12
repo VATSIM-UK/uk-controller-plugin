49530aded6d4308d94a4e0733ddfb80956c796b1
Andy Ford <andyford993@gmail.com>
2021-05-12 16:46:47 +0100

    feat(release): add departure releases

---
+++ src/releases/DepartureReleaseRequest.h
@@ -0,0 +1,71 @@
+#pragma once
+namespace UKControllerPlugin {
+    namespace Releases {
+
+        class DepartureReleaseRequest
+        {
+            public:
+                DepartureReleaseRequest(
+                    int id,
+                    std::string callsign,
+                    int requestingController,
+                    int targetController,
+                    std::chrono::system_clock::time_point requestExpiresAt
+                );
+
+                void Acknowledge();
+                void Reject();
+                void Approve(
+                    std::chrono::system_clock::time_point releasedAtTime,
+                    std::chrono::system_clock::time_point releaseExpiresAt
+                );
+
+                int Id() const;
+                std::string Callsign() const;
+                int RequestingController() const;
+                int TargetController() const;
+                bool Acknowledged() const;
+                bool Rejected() const;
+                bool Approved() const;
+                std::chrono::system_clock::time_point RequestExpiryTime() const;
+                std::chrono::system_clock::time_point ReleaseExpiryTime() const;
+                std::chrono::system_clock::time_point ReleasedAtTime() const;
+                std::chrono::system_clock::time_point RejectedAtTime() const;
+                std::chrono::system_clock::time_point AcknowledgedAtTime() const;
+
+            private:
+
+                static inline const std::chrono::system_clock::time_point noTime = (
+                    std::chrono::system_clock::time_point::min
+                )();
+
+                // The id of the request
+                int id;
+
+                // The callsign for the request
+                std::string callsign;
+
+                // Who's requesting the release
+                int requestingController;
+
+                // Who's the target of the release request
+                int targetController;
+
+                // When the the request expires
+                std::chrono::system_clock::time_point requestExpiresAt;
+
+                // What time the release was acknowledged
+                std::chrono::system_clock::time_point acknowledgedAtTime = noTime;
+
+                // What time the release was rejected
+                std::chrono::system_clock::time_point rejectedAtTime = noTime;
+
+                // The time the aircraft has been approved for release
+                std::chrono::system_clock::time_point releasedAtTime = noTime;
+
+                // The time that the release expires
+                std::chrono::system_clock::time_point releaseExpiresAt = noTime;
+        };
+
+    } // namespace Releases
+} // namespace UKControllerPlugin
---
+++ test/test/releases/DepartureReleaseRequestTest.cpp
@@ -0,0 +1,113 @@
+#pragma once
+#include "pch/pch.h"
+#include "releases/DepartureReleaseRequest.h"
+
+using UKControllerPlugin::Releases::DepartureReleaseRequest;
+using testing::Test;
+
+namespace UKControllerPluginTest {
+    namespace Releases {
+
+        class DepartureReleaseRequestTest : public Test
+        {
+            public:
+                DepartureReleaseRequestTest()
+                    : requestExpiresAt(std::chrono::system_clock::now() + std::chrono::minutes(3)),
+                      request(1, "BAW123", 2, 3, requestExpiresAt)
+                { }
+
+                std::chrono::system_clock::time_point requestExpiresAt;
+                DepartureReleaseRequest request;
+        };
+
+        TEST_F(DepartureReleaseRequestTest, ItHasAnId)
+        {
+            EXPECT_EQ(1, request.Id());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ItHasACallsign)
+        {
+            EXPECT_EQ("BAW123", request.Callsign());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ItHasARequestingController)
+        {
+            EXPECT_EQ(2, request.RequestingController());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ItHasATargetController)
+        {
+            EXPECT_EQ(3, request.TargetController());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ItHasARequestExpiryTime)
+        {
+            EXPECT_EQ(requestExpiresAt, request.RequestExpiryTime());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ItStartsNotAcknowledged)
+        {
+            EXPECT_FALSE(request.Acknowledged());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ItStartsNotApproved)
+        {
+            EXPECT_FALSE(request.Approved());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ItStartsNotRejected)
+        {
+            EXPECT_FALSE(request.Rejected());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ItCanBeAcknowledged)
+        {
+            request.Acknowledge();
+            EXPECT_TRUE(request.Acknowledged());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, AcknowledgingTheRequestSetsAcknowledgeTime)
+        {
+            request.Acknowledge();
+            EXPECT_TRUE(std::chrono::system_clock::now() - request.AcknowledgedAtTime() < std::chrono::seconds(5));
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ItCanBeRejected)
+        {
+            request.Reject();
+            EXPECT_TRUE(request.Rejected());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, RejectingTheRequestSetsRejectTime)
+        {
+            request.Reject();
+            EXPECT_TRUE(std::chrono::system_clock::now() - request.RejectedAtTime() < std::chrono::seconds(5));
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ItCanBeApproved)
+        {
+            request.Approve(std::chrono::system_clock::now(), std::chrono::system_clock::now());
+            EXPECT_TRUE(request.Approved());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ApprovingTheRequestSetsReleasedAtTime)
+        {
+            std::chrono::system_clock::time_point releasedAt = std::chrono::system_clock::now() +
+                std::chrono::minutes(1);
+            std::chrono::system_clock::time_point releaseExpiresAt = std::chrono::system_clock::now() +
+                std::chrono::minutes(2);
+            request.Approve(releasedAt, releaseExpiresAt);
+            EXPECT_EQ(releasedAt, request.ReleasedAtTime());
+        }
+
+        TEST_F(DepartureReleaseRequestTest, ApprovingTheRequestSetsReleaseExpiryTime)
+        {
+            std::chrono::system_clock::time_point releasedAt = std::chrono::system_clock::now() +
+                std::chrono::minutes(1);
+            std::chrono::system_clock::time_point releaseExpiresAt = std::chrono::system_clock::now() +
+                std::chrono::minutes(2);
+            request.Approve(releasedAt, releaseExpiresAt);
+            EXPECT_EQ(releaseExpiresAt, request.ReleaseExpiryTime());
+        }
+    } // namespace Releases
+} // namespace UKControllerPluginTest
