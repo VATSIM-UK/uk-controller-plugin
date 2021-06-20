b401faf817377070daf083ccac7f8df64bff3ac0
Andy Ford <andyford993@gmail.com>
2021-06-20 16:15:32 +0100

    Test Styles

--- test/test/releases/ToggleDepartureReleaseDecisionListTest.cpp
+++ test/test/releases/ToggleDepartureReleaseDecisionListTest.cpp
@@ -43,7 +43,6 @@ namespace UKControllerPluginTest {
                           )
                       ),
                       dialogManager(dialogProvider)
-                       
                 { }
 
                 ToggleDepartureReleaseDecisionList GetList(bool alreadyToggled)
