c64b2809c5c0e9d8a0ecc5044048b566a2c77913
Andy Ford <andyford993@gmail.com>
2021-06-27 17:04:36 +0100

    Add Interface For Outbound Message

---
+++ src/integration/OutboundMessageInterface.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "pch/stdafx.h"
+
+namespace UKControllerPlugin::Integration {
+
+    /*
+     * Represents a message that's outbound from the plugin to other
+     * interested plugins and programs.
+     */
+    class OutboundMessageInterface
+    {
+        public:
+            virtual ~OutboundMessageInterface() = default;
+
+            /*
+             * Get the data to go in the message.
+             */
+            virtual nlohmann::json GetMessageData() const = 0;
+
+            /*
+             * Get the version of the message.
+             */
+            virtual unsigned int GetMessageVersion() const = 0;
+
+            /*
+             * Get the type of the message.
+             */
+            virtual std::string GetMessageType() const = 0;
+    };
+} // namespace Integration
