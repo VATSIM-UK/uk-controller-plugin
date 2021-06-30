1cf8470f7927034ead1de93c5f454a7fa50fd024
Andy Ford <AndyTWF@users.noreply.github.com>
2021-06-30 17:58:27 +0100

    build(semantic): Fix semantic call in build version

--- .github/workflows/plugin_version.ps1
+++ .github/workflows/plugin_version.ps1
@@ -2,7 +2,7 @@ Function Build-Version {
     # If we're on main, invoke semantic release to get the next version
     if (${env:GITHUB_REF} -eq "refs/heads/main")
     {
-        $semanticRelease = npx -p @semantic-release/changelog semantic-release@17 --dry-run | Select-String -Pattern "Published release ((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)"
+        $semanticRelease = npx -p @semantic-release/changelog -p semantic-release@17 semantic-release --dry-run | Select-String -Pattern "Published release ((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)"
         if ($semanticRelease.Matches -eq $null)
         {
             return "non-release-build"
