d8b47b58a3b91fcf6f0265e1fe826d3735238bbb
Andy Ford <andyford993@gmail.com>
2021-07-30 14:58:25 +0100

    refactor(structure): Move utils test

---
+++ test/utils/setting/SettingRepositoryTest.cpp
@@ -0,0 +1,284 @@
+#include "pch/pch.h"
+#include "setting/SettingRepository.h"
+#include "setting/SettingValue.h"
+#include "mock/MockWinApi.h"
+
+using UKControllerPlugin::Setting::SettingRepository;
+using UKControllerPlugin::Setting::SettingValue;
+using UKControllerPluginTest::Windows::MockWinApi;
+using ::testing::StrictMock;
+using ::testing::Return;
+using ::testing::Throw;
+
+namespace UKControllerPluginUtilsTest {
+    namespace Setting {
+
+        TEST(SettingRepository, AddSettingValueAddsSetting)
+        {
+            StrictMock<MockWinApi> winApiMock;
+            SettingRepository repo(winApiMock);
+            repo.AddSettingValue({"foo", "bar", "baz"});
+
+            EXPECT_EQ(1, repo.SettingsCount());
+        }
+
+        TEST(SettingRepository, AddSettingValueDoesntOverwrite)
+        {
+            StrictMock<MockWinApi> winApiMock;
+            SettingRepository repo(winApiMock);
+            repo.AddSettingValue({"foo", "bar", "baz"});
+            repo.AddSettingValue({"foo", "bob", "barrie"});
+
+            EXPECT_EQ("bar", repo.GetSetting("foo"));
+            EXPECT_EQ(1, repo.SettingsCount());
+        }
+
+        TEST(SettingRepository, AddSettingsFromJsonFileStopsIfFileDoesntExist)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test.json")))
+                .Times(1)
+                .WillOnce(Return(false));
+
+            SettingRepository repo(winApiMock);
+            repo.AddSettingsFromJsonFile("test/test.json");
+            EXPECT_EQ(0, repo.SettingsCount());
+        }
+
+        TEST(SettingRepository, AddSettingsFromJsonFileStopsIfFileReadError)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test.json"), true))
+                .Times(1)
+                .WillOnce(Throw(std::ifstream::failure("test")));
+
+            SettingRepository repo(winApiMock);
+            repo.AddSettingsFromJsonFile("test/test.json");
+            EXPECT_EQ(0, repo.SettingsCount());
+        }
+
+        TEST(SettingRepository, AddSettingsFromJsonFileStopsIfInvalidJson)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test.json"), true))
+                .Times(1)
+                .WillOnce(Return("{{}"));
+
+            SettingRepository repo(winApiMock);
+            repo.AddSettingsFromJsonFile("test/test.json");
+            EXPECT_EQ(0, repo.SettingsCount());
+        }
+
+        TEST(SettingRepository, AddSettingsFromJsonFileStopsIfNotJsonObject)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test.json"), true))
+                .Times(1)
+                .WillOnce(Return("abcd"));
+
+            SettingRepository repo(winApiMock);
+            repo.AddSettingsFromJsonFile("test/test.json");
+            EXPECT_EQ(0, repo.SettingsCount());
+        }
+
+        TEST(SettingRepository, AddSettingsFromJsonFileAddsSettings)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test.json"), true))
+                .Times(1)
+                .WillOnce(Return("{\"test1\": \"testValue1\", \"test2\": \"testValue2\"}"));
+
+            SettingRepository repo(winApiMock);
+            repo.AddSettingsFromJsonFile("test/test.json");
+            EXPECT_EQ(2, repo.SettingsCount());
+            EXPECT_TRUE(repo.HasSetting("test1"));
+            EXPECT_TRUE(repo.HasSetting("test2"));
+            EXPECT_TRUE(repo.GetSetting("test1") == "testValue1");
+            EXPECT_TRUE(repo.GetSetting("test2") == "testValue2");
+        }
+
+        TEST(SettingRepository, AddSettingsFromJsonFileIgnoresDuplicateSettings)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test1.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test2.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test1.json"), true))
+                .Times(1)
+                .WillOnce(Return("{\"test1\": \"testValue1\"}"));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test2.json"), true))
+                .Times(1)
+                .WillOnce(Return("{\"test1\": \"testValue2\"}"));
+
+            SettingRepository repo(winApiMock);
+            repo.AddSettingsFromJsonFile("test/test1.json");
+            repo.AddSettingsFromJsonFile("test/test2.json");
+            EXPECT_TRUE(repo.HasSetting("test1"));
+            EXPECT_TRUE(repo.GetSetting("test1") == "testValue1");
+        }
+
+        TEST(SettingRepository, GetSettingReturnsEmptyStringByDefaultIfNotSet)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            SettingRepository repo(winApiMock);
+            EXPECT_TRUE("" == repo.GetSetting("test"));
+        }
+
+        TEST(SettingRepository, GetSettingReturnsDefaultvalueIfProvided)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            SettingRepository repo(winApiMock);
+            EXPECT_TRUE("test2" == repo.GetSetting("test", "test2"));
+        }
+
+        TEST(SettingRepository, UpdateSettingChangesSettingValue)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test.json"), true))
+                .Times(1)
+                .WillOnce(Return("{\"test1\": \"testValue1\", \"test2\": \"testValue2\"}"));
+
+            SettingRepository repo(winApiMock);
+            repo.AddSettingsFromJsonFile("test/test.json");
+            repo.UpdateSetting("test1", "notTestValue1");
+            EXPECT_TRUE(repo.HasSetting("test1"));
+            EXPECT_TRUE(repo.GetSetting("test1") == "notTestValue1");
+        }
+
+        TEST(SettingRepository, UpdateSettingDoesNothingIfSettingDoesNotExist)
+        {
+            StrictMock<MockWinApi> winApiMock;
+            SettingRepository repo(winApiMock);
+            repo.UpdateSetting("test1", "notTestValue1");
+            EXPECT_FALSE(repo.HasSetting("test2"));
+        }
+
+        TEST(SettingRepository, WriteSettingsToFileHandlesSingleFile)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test.json"), true))
+                .Times(1)
+                .WillOnce(Return("{\"test1\": \"testValue1\", \"test2\": \"testValue2\"}"));
+
+            EXPECT_CALL(
+                winApiMock,
+                WriteToFile(
+                    std::wstring(L"settings/test/test.json"),
+                    "{\n    \"test1\": \"testValue1\",\n    \"test2\": \"testValue2\"\n}", true, false
+                ))
+                .Times(1);
+
+            SettingRepository repo(winApiMock);
+            repo.AddSettingsFromJsonFile("test/test.json");
+            repo.WriteSettingsToFile();
+        }
+
+        TEST(SettingRepository, WriteSettingsToFileHandlesUpdatedSettings)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test.json"), true))
+                .Times(1)
+                .WillOnce(Return("{\"test1\": \"testValue1\", \"test2\": \"testValue2\"}"));
+
+            EXPECT_CALL(
+                winApiMock,
+                WriteToFile(
+                    std::wstring(L"settings/test/test.json"),
+                    "{\n    \"test1\": \"notTestValue1\",\n    \"test2\": \"testValue2\"\n}", true, false
+                ))
+                .Times(1);
+
+            SettingRepository repo(winApiMock);
+            repo.AddSettingsFromJsonFile("test/test.json");
+            repo.UpdateSetting("test1", "notTestValue1");
+            repo.WriteSettingsToFile();
+        }
+
+        TEST(SettingRepository, WriteSettingsToFileHandlesMultipleFiles)
+        {
+            StrictMock<MockWinApi> winApiMock;
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test1.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, FileExists(std::wstring(L"settings/test/test2.json")))
+                .Times(1)
+                .WillOnce(Return(true));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test1.json"), true))
+                .Times(1)
+                .WillOnce(Return("{\"test1\": \"testValue1\"}"));
+
+            EXPECT_CALL(winApiMock, ReadFromFileMock(std::wstring(L"settings/test/test2.json"), true))
+                .Times(1)
+                .WillOnce(Return("{\"test2\": \"testValue2\"}"));
+
+            EXPECT_CALL(
+                winApiMock,
+                WriteToFile(
+                    std::wstring(L"settings/test/test1.json"), "{\n    \"test1\": \"testValue1\"\n}", true, false
+                )
+            )
+                .Times(1);
+
+            EXPECT_CALL(
+                winApiMock,
+                WriteToFile(
+                    std::wstring(L"settings/test/test2.json"), "{\n    \"test2\": \"testValue2\"\n}", true, false
+                )
+            )
+                .Times(1);
+
+            SettingRepository repo(winApiMock);
+            repo.AddSettingsFromJsonFile("test/test1.json");
+            repo.AddSettingsFromJsonFile("test/test2.json");
+            repo.WriteSettingsToFile();
+        }
+    }  // namespace Setting
+}  // namespace UKControllerPluginUtilsTest
---
+++ test/utils/update/UpdateBinariesTest.cpp
@@ -0,0 +1,381 @@
+#include "pch/pch.h"
+#include "api/ApiException.h"
+#include "update/UpdateBinaries.h"
+#include "mock/MockWinApi.h"
+#include "mock/MockApiInterface.h"
+#include "mock/MockCurlApi.h"
+#include "curl/CurlRequest.h"
+#include "curl/CurlResponse.h"
+
+using testing::Test;
+using testing::NiceMock;
+using UKControllerPluginTest::Windows::MockWinApi;
+using UKControllerPluginTest::Api::MockApiInterface;
+using UKControllerPluginTest::Curl::MockCurlApi;
+using UKControllerPlugin::Curl::CurlRequest;
+using UKControllerPlugin::Curl::CurlResponse;
+
+namespace UKControllerPluginUtilsTest {
+    namespace Update {
+        class UpdateBinariesTest : public Test
+        {
+            public:
+                NiceMock<MockWinApi> mockWindows;
+                NiceMock<MockApiInterface> mockApi;
+                NiceMock<MockCurlApi> mockCurl;
+        };
+
+        TEST_F(UpdateBinariesTest, UpdateDataIsValidIfAllPresent)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.0"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            EXPECT_TRUE(UKControllerPlugin::UpdateDataValid(updateData));
+        }
+
+        TEST_F(UpdateBinariesTest, UpdateDataIsNotValidIfVersionMissing)
+        {
+            nlohmann::json updateData{
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            EXPECT_FALSE(UKControllerPlugin::UpdateDataValid(updateData));
+        }
+
+        TEST_F(UpdateBinariesTest, UpdateDataIsNotValidIfVersionNotString)
+        {
+            nlohmann::json updateData{
+                {"version", 123},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            EXPECT_FALSE(UKControllerPlugin::UpdateDataValid(updateData));
+        }
+
+        TEST_F(UpdateBinariesTest, UpdateDataIsNotValidIfUpdaterUrlMissing)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.0"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            EXPECT_FALSE(UKControllerPlugin::UpdateDataValid(updateData));
+        }
+
+        TEST_F(UpdateBinariesTest, UpdateDataIsNotValidIfUpdaterUrlNotString)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.0"},
+                {"updater_download_url", 123},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            EXPECT_FALSE(UKControllerPlugin::UpdateDataValid(updateData));
+        }
+
+        TEST_F(UpdateBinariesTest, UpdateDataIsNotValidIfCoreUrlMissing)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.0"},
+                {"updater_download_url", "foo"},
+                {"loader_download_url", "baz"},
+            };
+
+            EXPECT_FALSE(UKControllerPlugin::UpdateDataValid(updateData));
+        }
+
+        TEST_F(UpdateBinariesTest, UpdateDataIsNotValidIfCoreUrlNotString)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.0"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", 123},
+                {"loader_download_url", "baz"},
+            };
+
+            EXPECT_FALSE(UKControllerPlugin::UpdateDataValid(updateData));
+        }
+
+        TEST_F(UpdateBinariesTest, UpdateDataIsNotValidIfLoaderUrlMissing)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.0"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+            };
+
+            EXPECT_FALSE(UKControllerPlugin::UpdateDataValid(updateData));
+        }
+
+        TEST_F(UpdateBinariesTest, UpdateDataIsNotValidIfCoreLoaderUrlNotString)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.0"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", 123},
+            };
+
+            EXPECT_FALSE(UKControllerPlugin::UpdateDataValid(updateData));
+        }
+
+        TEST_F(UpdateBinariesTest, GetUpdateDataReturnsUpdateData)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.0"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            ON_CALL(this->mockApi, GetUpdateDetails())
+                .WillByDefault(testing::Return(updateData));
+
+            EXPECT_EQ(updateData, UKControllerPlugin::GetUpdateData(this->mockApi));
+        }
+
+        TEST_F(UpdateBinariesTest, GetUpdateDataThrowsExceptionIfUpdateDataInvalid)
+        {
+            nlohmann::json updateData{
+                {"version", 1234}, // Invalid
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            ON_CALL(this->mockApi, GetUpdateDetails())
+                .WillByDefault(testing::Return(updateData));
+
+            EXPECT_THROW(UKControllerPlugin::GetUpdateData(this->mockApi), std::exception);
+        }
+
+        TEST_F(UpdateBinariesTest, GetUpdateDataThrowsApiExceptionIfThrownByApi)
+        {
+            nlohmann::json updateData{
+                {"version", 1234}, // Invalid
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            ON_CALL(this->mockApi, GetUpdateDetails())
+                .WillByDefault(testing::Throw(UKControllerPlugin::Api::ApiException("foo")));
+
+            EXPECT_THROW(UKControllerPlugin::GetUpdateData(this->mockApi), UKControllerPlugin::Api::ApiException);
+        }
+
+        TEST_F(UpdateBinariesTest, ItUpdatesTheCoreLibrary)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.1"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            ON_CALL(this->mockApi, GetUpdateDetails())
+                .WillByDefault(testing::Return(updateData));
+
+            CurlRequest expectedRequest(
+                "bar",
+                CurlRequest::METHOD_GET
+            );
+            expectedRequest.SetMaxRequestTime(0);
+
+            CurlResponse response("3.0.1.core", false, 200);
+
+            EXPECT_CALL(this->mockCurl, MakeCurlRequest(expectedRequest))
+                .Times(1)
+                .WillOnce(testing::Return(response));
+
+
+            EXPECT_CALL(
+                this->mockWindows,
+                WriteToFile(std::wstring(L"bin/UKControllerPluginCore.dll"), "3.0.1.core", true, true)
+            )
+                .Times(1);
+
+            EXPECT_TRUE(UKControllerPlugin::DownloadCoreLibrary(updateData, this->mockWindows, this->mockCurl));
+        }
+
+        TEST_F(UpdateBinariesTest, ItHandlesCurlErrorsUpdatingTheCoreLibrary)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.1"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            ON_CALL(this->mockApi, GetUpdateDetails())
+                .WillByDefault(testing::Return(updateData));
+
+            CurlRequest expectedRequest(
+                "bar",
+                CurlRequest::METHOD_GET
+            );
+            expectedRequest.SetMaxRequestTime(0);
+
+            CurlResponse response("3.0.1.core", true, 200);
+
+            EXPECT_CALL(this->mockCurl, MakeCurlRequest(expectedRequest))
+                .Times(1)
+                .WillOnce(testing::Return(response));
+
+
+            EXPECT_CALL(
+                this->mockWindows,
+                WriteToFile(std::wstring(L"bin/UKControllerPluginCore.dll"), "3.0.1.core", true, true)
+            )
+                .Times(0);
+
+            EXPECT_FALSE(UKControllerPlugin::DownloadCoreLibrary(updateData, this->mockWindows, this->mockCurl));
+        }
+
+        TEST_F(UpdateBinariesTest, ItHandlesHttpErrorsUpdatingTheCoreLibrary)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.1"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            ON_CALL(this->mockApi, GetUpdateDetails())
+                .WillByDefault(testing::Return(updateData));
+
+            CurlRequest expectedRequest(
+                "bar",
+                CurlRequest::METHOD_GET
+            );
+            expectedRequest.SetMaxRequestTime(0);
+
+            CurlResponse response("3.0.1.core", false, 500);
+
+            EXPECT_CALL(this->mockCurl, MakeCurlRequest(expectedRequest))
+                .Times(1)
+                .WillOnce(testing::Return(response));
+
+
+            EXPECT_CALL(
+                this->mockWindows,
+                WriteToFile(std::wstring(L"bin/UKControllerPluginCore.dll"), "3.0.1.core", true, true)
+            )
+                .Times(0);
+
+            EXPECT_FALSE(UKControllerPlugin::DownloadCoreLibrary(updateData, this->mockWindows, this->mockCurl));
+        }
+
+        TEST_F(UpdateBinariesTest, ItUpdatesTheUpdaterLibrary)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.1"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            ON_CALL(this->mockApi, GetUpdateDetails())
+                .WillByDefault(testing::Return(updateData));
+
+            CurlRequest expectedRequest(
+                "foo",
+                CurlRequest::METHOD_GET
+            );
+            expectedRequest.SetMaxRequestTime(0);
+
+            CurlResponse response("3.0.1.updater", false, 200);
+
+            EXPECT_CALL(this->mockCurl, MakeCurlRequest(expectedRequest))
+                .Times(1)
+                .WillOnce(testing::Return(response));
+
+
+            EXPECT_CALL(
+                this->mockWindows,
+                WriteToFile(std::wstring(L"bin/UKControllerPluginUpdater.dll"), "3.0.1.updater", true, true)
+            )
+                .Times(1);
+
+            EXPECT_TRUE(UKControllerPlugin::DownloadUpdater(updateData, this->mockWindows, this->mockCurl));
+        }
+
+        TEST_F(UpdateBinariesTest, ItHandlesCurlErrorsUpdatingTheUpdaterLibrary)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.1"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            ON_CALL(this->mockApi, GetUpdateDetails())
+                .WillByDefault(testing::Return(updateData));
+
+            CurlRequest expectedRequest(
+                "foo",
+                CurlRequest::METHOD_GET
+            );
+            expectedRequest.SetMaxRequestTime(0);
+
+            CurlResponse response("3.0.1.updater", true, 200);
+
+            EXPECT_CALL(this->mockCurl, MakeCurlRequest(expectedRequest))
+                .Times(1)
+                .WillOnce(testing::Return(response));
+
+
+            EXPECT_CALL(
+                this->mockWindows,
+                WriteToFile(std::wstring(L"bin/UKControllerPluginCore.dll"), "3.0.1.updater", true, true)
+            )
+                .Times(0);
+
+            EXPECT_FALSE(UKControllerPlugin::DownloadUpdater(updateData, this->mockWindows, this->mockCurl));
+        }
+
+        TEST_F(UpdateBinariesTest, ItHandlesHttpErrorsUpdatingTheUpdaterLibrary)
+        {
+            nlohmann::json updateData{
+                {"version", "3.0.1"},
+                {"updater_download_url", "foo"},
+                {"core_download_url", "bar"},
+                {"loader_download_url", "baz"},
+            };
+
+            ON_CALL(this->mockApi, GetUpdateDetails())
+                .WillByDefault(testing::Return(updateData));
+
+            CurlRequest expectedRequest(
+                "foo",
+                CurlRequest::METHOD_GET
+            );
+            expectedRequest.SetMaxRequestTime(0);
+
+            CurlResponse response("3.0.1.updater", false, 500);
+
+            EXPECT_CALL(this->mockCurl, MakeCurlRequest(expectedRequest))
+                .Times(1)
+                .WillOnce(testing::Return(response));
+
+
+            EXPECT_CALL(
+                this->mockWindows,
+                WriteToFile(std::wstring(L"bin/UKControllerPluginCore.dll"), "3.0.1.updater", true, true)
+            )
+                .Times(0);
+
+            EXPECT_FALSE(UKControllerPlugin::DownloadUpdater(updateData, this->mockWindows, this->mockCurl));
+        }
+    } // namespace Update
+} // namespace UKControllerPluginUtilsTest
