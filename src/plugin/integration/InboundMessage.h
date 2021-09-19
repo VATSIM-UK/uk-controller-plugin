6fa89683650e720cc71139e51c4fe640f5b1834e
Andy Ford <andyford993@gmail.com>
2021-09-19 13:45:56 +0100

    More format

--- src/plugin/integration/InboundMessage.h
+++ src/plugin/integration/InboundMessage.h
@@ -9,24 +9,26 @@ namespace UKControllerPlugin::Integration {
     class InboundMessage : public MessageInterface
     {
         public:
-            [[nodiscard]] auto GetMessageData() const -> nlohmann::json override;
-            [[nodiscard]] auto GetMessageType() const -> MessageType override;
-            [[nodiscard]] auto GetMessageId() const -> std::string override;
-            static auto FromJson(const nlohmann::json& json) -> std::shared_ptr<InboundMessage>;
-            
-        protected:
-            static auto MessageFormatValid(const nlohmann::json& message) -> bool;
-            InboundMessage(std::string id, MessageType type, nlohmann::json::object_t data)
-                : type(std::move(type)), data(std::move(data)), id(std::move(id)) {}
-    
-        private:
-            // The type of message
-            MessageType type;
+        [[nodiscard]] auto GetMessageData() const -> nlohmann::json override;
+        [[nodiscard]] auto GetMessageType() const -> MessageType override;
+        [[nodiscard]] auto GetMessageId() const -> std::string override;
+        static auto FromJson(const nlohmann::json& json) -> std::shared_ptr<InboundMessage>;
 
-            // Data about the message
-            nlohmann::json::object_t data;
-            
-            // The id of the message
-            std::string id;
+        protected:
+        static auto MessageFormatValid(const nlohmann::json& message) -> bool;
+        InboundMessage(std::string id, MessageType type, nlohmann::json::object_t data)
+            : type(std::move(type)), data(std::move(data)), id(std::move(id))
+        {
+        }
+
+        private:
+        // The type of message
+        MessageType type;
+
+        // Data about the message
+        nlohmann::json::object_t data;
+
+        // The id of the message
+        std::string id;
     };
 } // namespace UKControllerPlugin::Integration
