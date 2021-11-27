6031fc13b335f41476214137b451e6f4215fff1f
Andy Ford <andyford993@gmail.com>
2021-11-27 15:07:08 +0000

    Time string parsing

--- src/plugin/time/ParseTimeStrings.cpp
+++ src/plugin/time/ParseTimeStrings.cpp
@@ -1,17 +1,20 @@
-#include "pch/pch.h"
-#include "time/ParseTimeStrings.h"
+#include "ParseTimeStrings.h"
 
-namespace UKControllerPlugin {
-    namespace Time {
+namespace UKControllerPlugin::Time {
 
-        const std::chrono::system_clock::time_point invalidTime = (std::chrono::system_clock::time_point::max)();
+    const std::chrono::system_clock::time_point invalidTime = (std::chrono::system_clock::time_point::max)();
+    const std::string FORMAT = "%F %T";
 
-        std::chrono::system_clock::time_point ParseTimeString(std::string time)
-        {
-            std::chrono::system_clock::time_point timePoint;
-            std::istringstream inputStream(time);
-            inputStream >> date::parse("%Y-%m-%d %H:%M:%S", timePoint);
-            return static_cast<bool>(inputStream) ? timePoint : invalidTime;
-        }
-    }  // namespace Time
-}  // namespace UKControllerPlugin
+    [[nodiscard]] auto ParseTimeString(const std::string& time) -> std::chrono::system_clock::time_point
+    {
+        std::chrono::system_clock::time_point timePoint;
+        std::istringstream inputStream(time);
+        inputStream >> date::parse(FORMAT, timePoint);
+        return static_cast<bool>(inputStream) ? timePoint : invalidTime;
+    }
+
+    auto ToDateTimeString(const std::chrono::system_clock::time_point& timePoint) -> std::string
+    {
+        return date::format(FORMAT, date::floor<std::chrono::seconds>(timePoint));
+    }
+} // namespace UKControllerPlugin::Time
