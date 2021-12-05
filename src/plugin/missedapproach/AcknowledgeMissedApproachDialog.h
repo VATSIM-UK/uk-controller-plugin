312011aed389999b514b8e377118049d9b9e9b46
Andy Ford <andyford993@gmail.com>
2021-12-05 17:45:54 +0000

    Format

--- src/plugin/missedapproach/AcknowledgeMissedApproachDialog.h
+++ src/plugin/missedapproach/AcknowledgeMissedApproachDialog.h
@@ -9,19 +9,17 @@ namespace UKControllerPlugin::MissedApproach {
     class AcknowledgeMissedApproachDialog
     {
         public:
-        AcknowledgeMissedApproachDialog(
-            std::shared_ptr<AcknowledgeMissedApproachTagFunction> tagFunction);
+        AcknowledgeMissedApproachDialog(std::shared_ptr<AcknowledgeMissedApproachTagFunction> tagFunction);
         static auto CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) -> LRESULT;
 
         private:
         auto PrivateWindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) -> LRESULT;
         auto InitDialog(HWND hwnd, LPARAM lParam) -> LRESULT;
         auto SaveDialog(HWND hwnd) -> LRESULT;
-        
+
         std::shared_ptr<AcknowledgeMissedApproachTagFunction> tagFunction;
-        
+
         private:
-        
         static inline const int REMARKS_BUFFER = 255;
     };
 } // namespace UKControllerPlugin::MissedApproach
