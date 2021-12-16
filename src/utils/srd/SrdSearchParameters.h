77fb94ed9976b108cae18a21fbdb938c1f299b79
Andy Ford <andyford993@gmail.com>
2021-12-16 16:18:29 +0000

    feat(srd): Prepopulate SRD search from flightplan

--- src/utils/srd/SrdSearchParameters.h
+++ src/utils/srd/SrdSearchParameters.h
@@ -11,5 +11,11 @@ namespace UKControllerPlugin::Srd {
         std::string destination;
 
         unsigned int requestedLevel = 0;
+
+        [[nodiscard]] auto operator==(const SrdSearchParameters& compare) const -> bool
+        {
+            return this->origin == compare.origin && this->destination == compare.destination &&
+                   this->requestedLevel == compare.requestedLevel;
+        }
     };
 } // namespace UKControllerPlugin::Srd
