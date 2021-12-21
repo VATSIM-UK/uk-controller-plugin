d42d706aa6c1b68ddd9ef9785eaf6547f3453c26
Andy Ford <andyford993@gmail.com>
2021-12-21 20:28:43 +0000

    Calculate the UK area exit point when doing intention codes
    This then allows us to do the SRD dialog prepopulate

--- src/plugin/intention/IntentionCodeGenerator.h
+++ src/plugin/intention/IntentionCodeGenerator.h
@@ -40,7 +40,10 @@ namespace UKControllerPlugin::IntentionCode {
         [[nodiscard]] static auto GetInvalidExitPointIndex() -> int;
 
         private:
-        auto FindFirExitPoint(UKControllerPlugin::Euroscope::EuroscopeExtractedRouteInterface& route) -> int;
+        [[nodiscard]] auto FoundAllExitPoints(const std::pair<int, int>& indexes) const -> bool;
+        void SetExitIndexes(std::pair<int, int>& indexes, int index) const;
+        auto FindFirExitPoint(UKControllerPlugin::Euroscope::EuroscopeExtractedRouteInterface& route)
+            -> std::pair<int, int>;
 
         std::string userControllerPosition;
 
@@ -55,6 +58,8 @@ namespace UKControllerPlugin::IntentionCode {
 
         // Used for the exit point index when its not a valid point on the route.
         static const int invalidExitPointIndex = -1;
+        
+        static constexpr std::pair<int, int> invalidExitPair = {invalidExitPointIndex, invalidExitPointIndex};
 
         // The value given to us by Euroscope if we try to find the time to a point that has been passed.
         const int exitPointPassed = -1;
