db9bdb83414f0a24484a82f2a9904ca57f6bb2e7
Andy Ford <andyford993@gmail.com>
2021-12-30 17:28:34 +0000

    Add new settings file for release channel

--- test/utils/setting/SettingRepositoryFactoryTest.cpp
+++ test/utils/setting/SettingRepositoryFactoryTest.cpp
@@ -7,23 +7,25 @@ using UKControllerPlugin::Setting::SettingRepository;
 using UKControllerPlugin::Setting::SettingRepositoryFactory;
 using UKControllerPluginTest::Windows::MockWinApi;
 
-namespace UKControllerPluginUtilsTest {
-    namespace Setting {
+namespace UKControllerPluginUtilsTest::Setting {
 
-        TEST(SettingRepositoryFactory, CreateLoadsApiConfigSettings)
-        {
-            StrictMock<MockWinApi> mockWinApi;
+    TEST(SettingRepositoryFactory, CreateLoadsApiConfigSettings)
+    {
+        StrictMock<MockWinApi> mockWinApi;
 
-            EXPECT_CALL(mockWinApi, FileExists(std::wstring(L"settings/api-settings.json")))
-                .Times(1)
-                .WillOnce(Return(true));
+        EXPECT_CALL(mockWinApi, FileExists(std::wstring(L"settings/api-settings.json")))
+            .Times(1)
+            .WillOnce(Return(true));
 
-            EXPECT_CALL(mockWinApi, ReadFromFileMock(std::wstring(L"settings/api-settings.json"), true))
-                .Times(1)
-                .WillOnce(Return("{\"api-key\": \"testkey\"}"));
+        EXPECT_CALL(mockWinApi, ReadFromFileMock(std::wstring(L"settings/api-settings.json"), true))
+            .Times(1)
+            .WillOnce(Return("{\"api-key\": \"testkey\"}"));
 
-            std::unique_ptr<SettingRepository> repo = SettingRepositoryFactory::Create(mockWinApi);
-            EXPECT_TRUE("testkey" == repo->GetSetting("api-key"));
-        }
-    } // namespace Setting
-} // namespace UKControllerPluginUtilsTest
+        EXPECT_CALL(mockWinApi, ReadFromFileMock(std::wstring(L"settings/release-channel.json"), true))
+            .Times(1)
+            .WillOnce(Return("{\"release_channel\": \"beta\"}"));
+
+        std::unique_ptr<SettingRepository> repo = SettingRepositoryFactory::Create(mockWinApi);
+        EXPECT_TRUE("beta" == repo->GetSetting("release_channel"));
+    }
+} // namespace UKControllerPluginUtilsTest::Setting
