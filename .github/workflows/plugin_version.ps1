2c4737b1900c6056b108a3bab685d9ebe933f88d
Andy Ford <AndyTWF@users.noreply.github.com>
2022-01-04 21:52:08 +0000

    Remove modifier

--- .github/workflows/plugin_version.ps1
+++ .github/workflows/plugin_version.ps1
@@ -2,7 +2,7 @@ Function Build-Version {
     # If we're on main, invoke semantic release to get the next version
     if (${env:GITHUB_REF} -eq "refs/heads/main")
     {
-        $semanticRelease = yarn semantic-release --dry-run | Select-String -Pattern "The next release version is (.*)\z"
+        $semanticRelease = yarn semantic-release --dry-run | Select-String -Pattern "The next release version is (.*)"
         if ($semanticRelease.Matches -eq $null)
         {
             return "non-release-build"
