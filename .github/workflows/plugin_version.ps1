e67d84b63570791922e6fa8ca8d7aa644b22bffc
Andy Ford <AndyTWF@users.noreply.github.com>
2022-03-03 20:26:06 +0000

    Untest

--- .github/workflows/plugin_version.ps1
+++ .github/workflows/plugin_version.ps1
@@ -2,8 +2,6 @@ Function Build-Version {
     # If we're on main, invoke semantic release to get the next version
     if (${env:GITHUB_REF} -eq "refs/heads/main" -or ${env:GITHUB_REF} -eq "refs/heads/alpha" -or ${env:GITHUB_REF} -eq "refs/heads/beta")
     {
-        yarn semantic-release --dry-run
-        asdfsadsa
         $semanticRelease = yarn semantic-release --dry-run | Select-String -Pattern "Published release ((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)"
         if ($semanticRelease.Matches -eq $null)
         {
