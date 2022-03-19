5ef0759c21c88fbd8c9d0aeb64fdb18e961a22ef
Andy Ford <andyford993@gmail.com>
2022-03-19 13:25:01 +0000

    Add missing distance

--- test/plugin/approach/AirfieldMinimumSeparationSelectorListTest.cpp
+++ test/plugin/approach/AirfieldMinimumSeparationSelectorListTest.cpp
@@ -30,6 +30,7 @@ namespace UKControllerPluginTest::Approach {
                 "3.0",
                 "4.0",
                 "5.0",
+                "6.0",
                 "7.0",
                 "8.0",
                 "9.0",
