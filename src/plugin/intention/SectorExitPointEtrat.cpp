270d57b3a264013e840ad58b61df6c7d01fa9236
Andy Ford <andyford993@gmail.com>
2022-04-08 17:40:32 +0100

    feat(intentioncode): E3 intention code
    Adds the code for certain arrivals.
    
    Resolves #458

--- src/plugin/intention/SectorExitPointEtrat.cpp
+++ src/plugin/intention/SectorExitPointEtrat.cpp
@@ -1,24 +1,22 @@
-#include "pch/pch.h"
 #include "intention/SectorExitPointEtrat.h"
 #include "euroscope/EuroscopeExtractedRouteInterface.h"
 
 using UKControllerPlugin::Euroscope::EuroscopeExtractedRouteInterface;
 
-namespace UKControllerPlugin {
-    namespace IntentionCode {
+namespace UKControllerPlugin::IntentionCode {
 
-        std::string SectorExitPointEtrat::GetIntentionCode(
-            EuroscopeExtractedRouteInterface & route,
-            int foundPointIndex,
-            int cruiseLevel
-        ) const
-        {
-            // Special low level rule for ETRAT
-            if (cruiseLevel <= 27000) {
-                return "E2";
-            }
-
-            return SectorExitPoint::GetIntentionCode(route, foundPointIndex, cruiseLevel);
+    std::string SectorExitPointEtrat::GetIntentionCode(
+        EuroscopeExtractedRouteInterface& route, int foundPointIndex, int cruiseLevel) const
+    {
+        if (destinations.find(route.GetPointName(route.GetPointsNumber() - 1)) != destinations.cend()) {
+            return "E3";
         }
-    }  // namespace IntentionCode
-}  // namespace UKControllerPlugin
+
+        // Special low level rule for ETRAT
+        if (cruiseLevel <= 27000) {
+            return "E2";
+        }
+
+        return SectorExitPoint::GetIntentionCode(route, foundPointIndex, cruiseLevel);
+    }
+} // namespace UKControllerPlugin::IntentionCode
--- test/plugin/intention/SectorExitPointEtratTest.cpp
+++ test/plugin/intention/SectorExitPointEtratTest.cpp
@@ -1,50 +1,69 @@
 #include "intention/SectorExitPointEtrat.h"
 
-using ::testing::StrictMock;
 using UKControllerPlugin::IntentionCode::SectorExitPointEtrat;
 using UKControllerPluginTest::Euroscope::MockEuroscopeExtractedRouteInterface;
 
-namespace UKControllerPluginTest {
-    namespace IntentionCode {
+namespace UKControllerPluginTest::IntentionCode {
 
-        TEST(SectorExitPointEtrat, ItAppliesToAllControllers)
+    class SectorExitPointEtratTest : public testing::Test
+    {
+        public:
+        SectorExitPointEtratTest() : exitPoint("ETRAT", "E", SectorExitPointEtrat::outSouth)
         {
-            StrictMock<MockEuroscopeExtractedRouteInterface> routeMock;
-            SectorExitPointEtrat exitPoint("ETRAT", "E", SectorExitPointEtrat::outSouth);
-
-            EXPECT_TRUE(exitPoint.AppliesToController("BLA"));
+            ON_CALL(routeMock, GetPointsNumber()).WillByDefault(testing::Return(1));
+            ON_CALL(routeMock, GetPointName(0)).WillByDefault(testing::Return("EDDF"));
         }
 
-        TEST(SectorExitPointEtrat, GetIntentionCodeReturnsCorrectSecondaryCode)
-        {
-            StrictMock<MockEuroscopeExtractedRouteInterface> routeMock;
-            SectorExitPointEtrat exitPoint("ETRAT", "E", SectorExitPointEtrat::outSouth);
+        testing::NiceMock<MockEuroscopeExtractedRouteInterface> routeMock;
+        SectorExitPointEtrat exitPoint;
+    };
 
-            EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 25000).compare("E2"));
-        }
+    TEST_F(SectorExitPointEtratTest, ItAppliesToAllControllers)
+    {
+        EXPECT_TRUE(exitPoint.AppliesToController("BLA"));
+    }
 
-        TEST(SectorExitPointEtrat, GetIntentionCodeReturnsCorrectSecondaryBoundary)
-        {
-            StrictMock<MockEuroscopeExtractedRouteInterface> routeMock;
-            SectorExitPointEtrat exitPoint("ETRAT", "E", SectorExitPointEtrat::outSouth);
+    TEST_F(SectorExitPointEtratTest, GetIntentionCodeReturnsCorrectSecondaryCode)
+    {
+        EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 25000).compare("E2"));
+    }
 
-            EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 27000).compare("E2"));
-        }
+    TEST_F(SectorExitPointEtratTest, GetIntentionCodeReturnsCorrectSecondaryBoundary)
+    {
+        EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 27000).compare("E2"));
+    }
 
-        TEST(SectorExitPointEtrat, GetIntentionCodeReturnsCorrectPrimaryCodeBoundary)
-        {
-            StrictMock<MockEuroscopeExtractedRouteInterface> routeMock;
-            SectorExitPointEtrat exitPoint("ETRAT", "E", SectorExitPointEtrat::outSouth);
+    TEST_F(SectorExitPointEtratTest, GetIntentionCodeReturnsCorrectPrimaryCodeBoundary)
+    {
+        EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 27001).compare("E"));
+    }
 
-            EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 27001).compare("E"));
-        }
+    TEST_F(SectorExitPointEtratTest, GetIntentionCodeReturnsCorrectPrimaryCode)
+    {
+        EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 37000).compare("E"));
+    }
 
-        TEST(SectorExitPointEtrat, GetIntentionCodeReturnsCorrectPrimaryCode)
-        {
-            StrictMock<MockEuroscopeExtractedRouteInterface> routeMock;
-            SectorExitPointEtrat exitPoint("ETRAT", "E", SectorExitPointEtrat::outSouth);
+    TEST_F(SectorExitPointEtratTest, GetIntentionCodeReturnsCorrectCodeForDeauville)
+    {
+        ON_CALL(routeMock, GetPointName(0)).WillByDefault(testing::Return("LFRG"));
+        EXPECT_EQ("E3", exitPoint.GetIntentionCode(routeMock, 0, 25000));
+    }
 
-            EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 37000).compare("E"));
-        }
-    } // namespace IntentionCode
-} // namespace UKControllerPluginTest
+    TEST_F(SectorExitPointEtratTest, GetIntentionCodeReturnsCorrectCodeForCaen)
+    {
+        ON_CALL(routeMock, GetPointName(0)).WillByDefault(testing::Return("LFRK"));
+        EXPECT_EQ("E3", exitPoint.GetIntentionCode(routeMock, 0, 25000));
+    }
+
+    TEST_F(SectorExitPointEtratTest, GetIntentionCodeReturnsCorrectCodeForLeHavre)
+    {
+        ON_CALL(routeMock, GetPointName(0)).WillByDefault(testing::Return("LFOH"));
+        EXPECT_EQ("E3", exitPoint.GetIntentionCode(routeMock, 0, 25000));
+    }
+
+    TEST_F(SectorExitPointEtratTest, GetIntentionCodeReturnsCorrectCodeForEvreux)
+    {
+        ON_CALL(routeMock, GetPointName(0)).WillByDefault(testing::Return("LFOE"));
+        EXPECT_EQ("E3", exitPoint.GetIntentionCode(routeMock, 0, 25000));
+    }
+} // namespace UKControllerPluginTest::IntentionCode
