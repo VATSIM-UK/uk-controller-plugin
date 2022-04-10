a52340197df453f69e6cc1269f7c8960b6dc1a08
Andy Ford <andyford993@gmail.com>
2022-04-10 15:47:38 +0100

    Test fixes

--- test/plugin/intention/SectorExitPointLelnaTest.cpp
+++ test/plugin/intention/SectorExitPointLelnaTest.cpp
@@ -25,41 +25,41 @@ namespace UKControllerPluginTest::IntentionCode {
 
     TEST_F(SectorExitPointLelnaTest, GetIntentionCodeReturnsCorrectDefaultCodeLelna)
     {
-        EXPECT_CALL(routeMock, GetPointsNumber()).Times(1).WillRepeatedly(Return(2));
-        EXPECT_CALL(routeMock, GetPointName(0)).Times(3).WillRepeatedly(Return("LELNA"));
-        EXPECT_CALL(routeMock, GetPointName(1)).Times(4).WillRepeatedly(Return("LFMN"));
+        ON_CALL(routeMock, GetPointsNumber()).WillByDefault(Return(2));
+        ON_CALL(routeMock, GetPointName(0)).WillByDefault(Return("LELNA"));
+        ON_CALL(routeMock, GetPointName(1)).WillByDefault(Return("LFMN"));
         EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 37000).compare("H8"));
     }
 
     TEST_F(SectorExitPointLelnaTest, GetIntentionCodeReturnsCorrectSecondaryCodeArree)
     {
-        EXPECT_CALL(routeMock, GetPointsNumber()).Times(1).WillRepeatedly(Return(3));
-        EXPECT_CALL(routeMock, GetPointName(0)).Times(3).WillRepeatedly(Return("LELNA"));
-        EXPECT_CALL(routeMock, GetPointName(1)).Times(3).WillRepeatedly(Return("ARE"));
-        EXPECT_CALL(routeMock, GetPointName(2)).Times(1).WillRepeatedly(Return("LEMD"));
+        ON_CALL(routeMock, GetPointsNumber()).WillByDefault(Return(3));
+        ON_CALL(routeMock, GetPointName(0)).WillByDefault(Return("LELNA"));
+        ON_CALL(routeMock, GetPointName(1)).WillByDefault(Return("ARE"));
+        ON_CALL(routeMock, GetPointName(2)).WillByDefault(Return("LEMD"));
         EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 37000).compare("H3"));
     }
 
     TEST_F(SectorExitPointLelnaTest, GetIntentionCodeReturnsCorrectSecondaryCodeDomok)
     {
-        EXPECT_CALL(routeMock, GetPointsNumber()).Times(1).WillRepeatedly(Return(3));
-        EXPECT_CALL(routeMock, GetPointName(0)).Times(3).WillRepeatedly(Return("LELNA"));
-        EXPECT_CALL(routeMock, GetPointName(1)).Times(2).WillRepeatedly(Return("DOMOK"));
-        EXPECT_CALL(routeMock, GetPointName(2)).Times(1).WillRepeatedly(Return("LEMD"));
+        ON_CALL(routeMock, GetPointsNumber()).WillByDefault(Return(3));
+        ON_CALL(routeMock, GetPointName(0)).WillByDefault(Return("LELNA"));
+        ON_CALL(routeMock, GetPointName(1)).WillByDefault(Return("DOMOK"));
+        ON_CALL(routeMock, GetPointName(2)).WillByDefault(Return("LEMD"));
         EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 37000).compare("H3"));
     }
 
     TEST_F(SectorExitPointLelnaTest, GetIntentionCodeReturnsCorrectIntentionCodeForBrestFirArrivals)
     {
-        EXPECT_CALL(routeMock, GetPointsNumber()).Times(1).WillRepeatedly(Return(3));
-        EXPECT_CALL(routeMock, GetPointName(2)).Times(1).WillRepeatedly(Return("LFRO"));
+        ON_CALL(routeMock, GetPointsNumber()).WillByDefault(Return(3));
+        ON_CALL(routeMock, GetPointName(2)).WillByDefault(Return("LFRO"));
         EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 37000).compare("H5"));
     }
 
     TEST_F(SectorExitPointLelnaTest, GetIntentionCodeReturnsCorrectIntentionCodeForCherbourgArrivals)
     {
-        EXPECT_CALL(routeMock, GetPointsNumber()).Times(1).WillRepeatedly(Return(3));
-        EXPECT_CALL(routeMock, GetPointName(2)).Times(1).WillRepeatedly(Return("LFRC"));
+        ON_CALL(routeMock, GetPointsNumber()).WillByDefault(Return(3));
+        ON_CALL(routeMock, GetPointName(2)).WillByDefault(Return("LFRC"));
         EXPECT_EQ(0, exitPoint.GetIntentionCode(routeMock, 0, 25000).compare("JC"));
     }
 } // namespace UKControllerPluginTest::IntentionCode
