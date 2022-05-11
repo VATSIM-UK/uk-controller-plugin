5dade92d97451ee3fd6f1a87fcd3041ff9878e7f
Andy Ford <AndyTWF@users.noreply.github.com>
2022-05-11 14:27:23 +0100

    Update test

--- test/plugin/flightinformationservice/FlightInformationServiceTagItemTest.cpp
+++ test/plugin/flightinformationservice/FlightInformationServiceTagItemTest.cpp
@@ -108,32 +108,32 @@ namespace UKControllerPluginTest {
         TEST_F(FlightInformationServiceTagItemTest, ItPopulatesTheSelectionMenu)
         {
             PopupMenuItem menuItemBasic;
-            menuItemBasic.firstValue = "BASIC";
-            menuItemBasic.secondValue = "";
+            menuItemBasic.firstValue = "BS";
+            menuItemBasic.secondValue = "Basic";
             menuItemBasic.callbackFunctionId = 55;
             menuItemBasic.checked = EuroScopePlugIn::POPUP_ELEMENT_NO_CHECKBOX;
             menuItemBasic.disabled = false;
             menuItemBasic.fixedPosition = false;
 
             PopupMenuItem menuItemTraffic;
-            menuItemTraffic.firstValue = "TFC";
-            menuItemTraffic.secondValue = "";
+            menuItemTraffic.firstValue = "TS";
+            menuItemTraffic.secondValue = "Traffic";
             menuItemTraffic.callbackFunctionId = 55;
             menuItemTraffic.checked = EuroScopePlugIn::POPUP_ELEMENT_NO_CHECKBOX;
             menuItemTraffic.disabled = false;
             menuItemTraffic.fixedPosition = false;
 
             PopupMenuItem menuItemDecon;
-            menuItemDecon.firstValue = "DECON";
-            menuItemDecon.secondValue = "";
+            menuItemDecon.firstValue = "DS";
+            menuItemDecon.secondValue = "Deconfliction";
             menuItemDecon.callbackFunctionId = 55;
             menuItemDecon.checked = EuroScopePlugIn::POPUP_ELEMENT_NO_CHECKBOX;
             menuItemDecon.disabled = false;
             menuItemDecon.fixedPosition = false;
 
             PopupMenuItem menuItemProcedural;
-            menuItemProcedural.firstValue = "PROC";
-            menuItemProcedural.secondValue = "";
+            menuItemProcedural.firstValue = "PS";
+            menuItemProcedural.secondValue = "Procedural";
             menuItemProcedural.callbackFunctionId = 55;
             menuItemProcedural.checked = EuroScopePlugIn::POPUP_ELEMENT_NO_CHECKBOX;
             menuItemProcedural.disabled = false;
