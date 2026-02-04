bed6cc854f54aebbd0cf0441aa2e6c92c629555f
Daniel 'MrAdder' Green <dgreen03@gmail.com>
2026-02-04 08:38:35 +0000

    Update OceanicEventHandler.cpp

--- src/plugin/oceanic/OceanicEventHandler.cpp
+++ src/plugin/oceanic/OceanicEventHandler.cpp
@@ -43,8 +43,11 @@ namespace UKControllerPlugin::Oceanic {
                     return;
 
                 // Uppercase helper for matching
-                auto toUpper = [](std::string s) {
-                    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::toupper(c); });
+
+                auto upper = [](std::string s) {
+                    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {
+                        return static_cast<char>(std::toupper(c));
+                    });
                     return s;
                 };
 
