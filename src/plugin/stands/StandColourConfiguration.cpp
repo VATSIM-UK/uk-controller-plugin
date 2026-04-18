00d66cd832effbbbfabeaf1a5ac2d7e7e5c61020
Daniel Green <dgreen03@gmail.com>
2026-04-18 11:09:35 +0100

    Use DEFAULT_COLOUR for Unknown stand source
    Replace DEFAULT_UNKNOWN_COLOUR with the shared DEFAULT_COLOUR for the Unknown StandAssignment source and remove the now-unused DEFAULT_UNKNOWN_COLOUR constant. This unifies the default gray value and reduces duplicated color definitions (updated StandColourConfiguration.cpp and StandColourConfiguration.h).

--- src/plugin/stands/StandColourConfiguration.cpp
+++ src/plugin/stands/StandColourConfiguration.cpp
@@ -18,7 +18,7 @@ namespace UKControllerPlugin::Stands {
 
     StandColourConfiguration::StandColourConfiguration(UKControllerPlugin::Euroscope::UserSetting* userSetting)
         : userSetting(userSetting), sourceColours(
-                                        {{StandAssignment::Source::Unknown, DEFAULT_UNKNOWN_COLOUR},
+                                        {{StandAssignment::Source::Unknown, DEFAULT_COLOUR},
                                          {StandAssignment::Source::User, DEFAULT_USER_COLOUR},
                                          {StandAssignment::Source::ReservationAllocator, DEFAULT_RESERVATION_COLOUR},
                                          {StandAssignment::Source::VaaAllocator, DEFAULT_VAA_COLOUR},
--- src/plugin/stands/StandColourConfiguration.h
+++ src/plugin/stands/StandColourConfiguration.h
@@ -44,7 +44,6 @@ namespace UKControllerPlugin::Stands {
         static constexpr COLORREF DEFAULT_COLOUR = RGB(180, 180, 180);
 
         // Default colours if not configured
-        static constexpr COLORREF DEFAULT_UNKNOWN_COLOUR = RGB(204, 204, 204);   // grey
         static constexpr COLORREF DEFAULT_USER_COLOUR = RGB(255, 255, 255);      // white
         static constexpr COLORREF DEFAULT_RESERVATION_COLOUR = RGB(255, 216, 0); // yellow
         static constexpr COLORREF DEFAULT_VAA_COLOUR = RGB(255, 149, 41);        // orange
