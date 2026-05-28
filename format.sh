3dda4c7b942ef7385a6c48bd3ec4711740488812
Kristián Kunc <kristian.kunc@gmail.com>
2026-05-28 17:21:26 +0000

    accept target branch for formatter script

--- format.sh
+++ format.sh
@@ -1,5 +1,13 @@
 # !/bin/bash
 
-git diff --name-only --diff-filter=AM origin/main \
+# Usage: ./format.sh [target_branch] - when no target branch is provided, it defaults to "origin/main"
+
+# Example: 
+# ./format.sh
+# ./format.sh origin/develop
+
+TARGET_BRANCH="${1:-origin/main}"
+
+git diff --name-only --diff-filter=AM "$TARGET_BRANCH" \
 | grep -E '\.(c|h|cpp|hpp|cc|cxx)$' \
 | xargs -r clang-format-21 --style=file -i
